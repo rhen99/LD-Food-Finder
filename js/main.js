8ca5160404b6da15c48748d607c2ad5a6487f9c0
Arhen Santiago <lincoldarhen9900@gmail.com>
2018-08-13 14:56:11 +0800

    Bug fixed.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-alert('This is a PC only app. (does not work for smartphones)');
+
 if("geolocation" in navigator){
 
     function geo_success(position){
-        let find = document.getElementById('controlsForm');
+        const find = document.getElementById('controlsForm');
 
         find.addEventListener('submit', function(e){
             e.preventDefault();
@@ -12,21 +12,21 @@ if("geolocation" in navigator){
         
         
         function initialize() {
-            let mapdiv = document.getElementById('map');
-            let pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude );
-            let map = new google.maps.Map(mapdiv, {
+            const mapdiv = document.getElementById('map');
+            const pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude );
+            const map = new google.maps.Map(mapdiv, {
                 center: pos,
                 zoom: 11
             });
 
-            let me = new google.maps.Marker({
+            const me = new google.maps.Marker({
                 position: pos,
                 title: 'You'
             });
             me.setMap(map);
             
-            let infowindow = new google.maps.InfoWindow();
-            let service = new google.maps.places.PlacesService(map);
+            const infowindow = new google.maps.InfoWindow();
+            const service = new google.maps.places.PlacesService(map);
             service.nearbySearch({
                 location: pos,
                 radius: document.getElementById('radius').value,
@@ -38,16 +38,16 @@ if("geolocation" in navigator){
             function callback(results, status){
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
                     console.log(results);
-                    for (let i = 0; i < results.length; i++) {
+                    for (const i = 0; i < results.length; i++) {
                         addMarker(results[i]);
         
                     }
                 }
             }
             function addMarker(place){
-                let placeLoc = place.geometry.location;
-                let icon = place.icon;
-                let marker = new google.maps.Marker({
+                const placeLoc = place.geometry.location;
+                const icon = place.icon;
+                const marker = new google.maps.Marker({
                     map: map,
                     position: placeLoc,
                     icon: icon
@@ -66,13 +66,13 @@ if("geolocation" in navigator){
     function geo_error(){
         alert('Position not found.');
     }
-    var geo_options = {
+    const geo_options = {
         enableHighAccuracy: true,
         maximumAge: 27000,
         timeout: 30000
 
     }
-    var wpid = navigator.geolocation.getCurrentPosition(geo_success, geo_error, geo_options);
+    const wpid = navigator.geolocation.getCurrentPosition(geo_success, geo_error, geo_options);
 
     
     
